6916aea8fc61053dca2c578eb2e7c196de6a6a05
Felipe Alves de Souza <felipe.souza@ax4b.com>
2022-01-14 17:55:07 -0300

    Adicionar arquivos de projeto.

---
+++ DioSeries/Enum/Genero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DioSeries
+{
+    public enum Genero
+    {
+        Açao = 1,
+
+        Aventura = 2, 
+
+        Comedia = 3,
+
+        Documentario = 4,
+
+        Romance = 5,
+
+        Terror = 6,
+
+        Suspense = 7,
+    }
+}
